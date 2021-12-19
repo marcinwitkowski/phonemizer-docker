d94dbfad6ba2a738a7aed4568e93c976d5916222
jlalus99 <jakub.la1511@gmail.com>
2021-12-20 00:52:56 +0100

    Update configuration.sh

--- configuration.sh
+++ configuration.sh
@@ -1,11 +1,11 @@
 #git clone https://github.com/jlalus/phonemizer-docker
 cd ~
 git clone https://github.com/bootphon/phonemizer
-cd phonemizer
-python setup.py install
+
 cp phonemizer-docker/Dockerfile phonemizer/Dockerfile
 cp phonemizer-docker/app.py phonemizer/app.py
 
+cd phonemizer
 
 sudo docker build -t phonemizer .
 
