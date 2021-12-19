f130e8a812ffc73b96c82c5e3542816864947260
jlalus99 <jakub.la1511@gmail.com>
2021-12-20 00:00:44 +0100

    Update app.py
    Add function and shell operator

--- app.py
+++ app.py
@@ -1,34 +1,41 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import os
+import re
 from flask import Flask, request, jsonify
-from phonemizer.backend import EspeakBackend
-from phonemizer.punctuation import Punctuation
-from phonemizer.separator import Separator
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    txt = "złodziej"
-    return jsonify({"output-text": use_phonemizer(txt)})
+    txt = "test"
+    return jsonify({"output-text": txt})
 
 
-@app.route('/json', methods=['POST'])
-def use_phonemizer(txt):
-    text = Punctuation(';:,.!"?()-').remove(txt)
-    words = {w.lower() for line in text for w in line.strip().split(' ') if w}
-    backend = EspeakBackend('pl')
-    separator = Separator(phone=' ', word=None)
+@app.route('/post', methods=['POST'])
+def post_route():
+    if request.method == 'POST':
 
-    # build the lexicon by phonemizing each word one by one. The backend.phonemize
-    # function expect a list as input and outputs a list.
-    lexicon = {
-        word: backend.phonemize([word], separator=separator, strip=True)[0]
-        for word in words}
+        data = request.get_json(force=True)
+        txt = data['text']
+
+        out = []
+        for t in txt:
+            temp = use_phonemizer(t)
+            temp = re.sub(r'[^\w]', ' ', temp)
+            out.append(temp)
+        print('Data Received: "{data}"'.format(data=data))
+        return jsonify({"output-text": out})
+
+
+def use_phonemizer(txt, language="pl", backend='espeak'):
+    shell_cmd = f"\
+        echo '{txt}' | phonemize -b {backend} -l {language} -p ' ' -w '|' --words-mismatch ignore \
+    "
+    output = os.popen(shell_cmd).read()
+    return output
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+
+# curl 172.17.0.2:5000/post -d '{"text": ["Andrzej", "traktor", "Szekspir"]}' -H 'Content-Type: application/json'
